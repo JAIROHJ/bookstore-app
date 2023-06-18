5eabbb8c411b55f1d85bc4fad8b512091e52661e
JAIROHJ <65579713+JAIROHJ@users.noreply.github.com>
2023-06-18 15:46:49 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import BookStoreProvider from "./context/BookStoreProvider";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BookStoreProvider>
+
     <App />
+    </BookStoreProvider>
   </React.StrictMode>
 );
 
---
+++ src/pages/about/About.jsx
@@ -0,0 +1,15 @@
+import "./about.css";
+
+const About = () => {
+  return (
+    <section className="about">
+      <h1>About Us</h1>
+      <p>We are an online book store</p>
+      <p>
+        <strong>Please Visit BookStore App</strong>
+      </p>
+    </section>
+  );
+};
+
+export default About;
